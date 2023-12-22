7f6278c70083c6fa5a47048665b216fcd2b6dd41
FerdiStro <ferdistr08@gmai.com>
2023-12-22 13:56:47 +0100

    update deps

--- src/main/java/pack1/Var.java
+++ src/main/java/pack1/Var.java
@@ -80,65 +80,65 @@ public class Var {
 		try {
 			
 			//BOSS
-			iboss1background = ImageIO.read(new File("rsc/Hintergrund/Boss1.png"));
-			ighead = ImageIO.read(new File("rsc/Boss/Head.png"));
-			igbody = ImageIO.read(new File("rsc/Boss/Body.png"));
-			igarm1 = ImageIO.read(new File("rsc/Boss/Arm1.png")); igarm2 = ImageIO.read(new File ("rsc/Boss/Arm2.png"));
-			igarmr1 = ImageIO.read(new File("rsc/Boss/Armr1.png")); igarmr2 = ImageIO.read(new File("rsc/Boss/Armr2.png"));
-			igbein1 = ImageIO.read(new File("rsc/Boss/Bein1.png")); igbein2 = ImageIO.read(new File ("rsc/Boss/Bein2.png"));
-			igbeinr1= ImageIO.read(new File("rsc/Boss/Beinr1.png")); igbeinr2 = ImageIO.read(new File("rsc/Boss/Beinr2.png"));
-			iwaffe1 = ImageIO.read(new File("rsc/Boss/Waffe1.png"));
+			iboss1background = ImageIO.read(new File("src/main/resources/Hintergrund/Boss1.png"));
+			ighead = ImageIO.read(new File("src/main/resources/Boss/Head.png"));
+			igbody = ImageIO.read(new File("src/main/resources/Boss/Body.png"));
+			igarm1 = ImageIO.read(new File("src/main/resources/Boss/Arm1.png")); igarm2 = ImageIO.read(new File ("src/main/resources/Boss/Arm2.png"));
+			igarmr1 = ImageIO.read(new File("src/main/resources/Boss/Armr1.png")); igarmr2 = ImageIO.read(new File("src/main/resources/Boss/Armr2.png"));
+			igbein1 = ImageIO.read(new File("src/main/resources/Boss/Bein1.png")); igbein2 = ImageIO.read(new File ("src/main/resources/Boss/Bein2.png"));
+			igbeinr1= ImageIO.read(new File("src/main/resources/Boss/Beinr1.png")); igbeinr2 = ImageIO.read(new File("src/main/resources/Boss/Beinr2.png"));
+			iwaffe1 = ImageIO.read(new File("src/main/resources/Boss/Waffe1.png"));
 			//BOSS//Effecte
-			iwolke1 = ImageIO.read(new File("rsc/PatickelEffeckte/Wolken1.png"));
-			iwolke2 = ImageIO.read(new File("rsc/PatickelEffeckte/Wolken2.png"));
+			iwolke1 = ImageIO.read(new File("src/main/resources/PatickelEffeckte/Wolken1.png"));
+			iwolke2 = ImageIO.read(new File("src/main/resources/PatickelEffeckte/Wolken2.png"));
 			//Boss Label
-			ibossback = ImageIO.read(new File("rsc/Hintergrund/Transperenz.png"));
+			ibossback = ImageIO.read(new File("src/main/resources/Hintergrund/Transperenz.png"));
 
 			//Menu
-			iheadline = ImageIO.read(new File("rsc/Menu/Headline.png"));
-			
-			ibackM = ImageIO.read(getClass().getResource("/main/resources/Menu/HintergrundM.png"));
-			istart1 = ImageIO.read(new File("rsc/Menu/Start2.png")); 
-			ioptionen = ImageIO.read(getClass().getResource("/main/resources/Menu/Optionen.png"));
-			ioptionen1 = ImageIO.read(new File("rsc/Menu/Optionen2.png"));
+			iheadline = ImageIO.read(new File("src/main/resources/Menu/Headline.png"));
+
+			ibackM = ImageIO.read(new File("src/main/resources/Menu/HintergrundM.png"));
+			istart1 = ImageIO.read(new File("src/main/resources/Menu/Start2.png"));
+			ioptionen = ImageIO.read(new File("src/main/resources/Menu/Optionen.png"));
+			ioptionen1 = ImageIO.read(new File("src/main/resources/Menu/Optionen2.png"));
 			// background
-			ib1 = ImageIO.read(new File("rsc/b1.0.png"));
-			ib2 = ImageIO.read(new File("rsc/b1.1.png"));
-			//Player 
-			ihead = ImageIO.read(new File("rsc/Head.png"));
-			ibody = ImageIO.read(new File("rsc/Body.png"));
-			iarm1 = ImageIO.read(new File("rsc/Arm1.png")); iarm2 = ImageIO.read(new File ("rsc/Arm2.png"));
-			iarmr1 = ImageIO.read(new File("rsc/Armr1.png")); iarmr2 = ImageIO.read(new File("rsc/Armr2.png"));
-			ibein1 = ImageIO.read(new File("rsc/Bein1.png")); ibein2 = ImageIO.read(new File ("rsc/Bein2.png"));
-			ibeinr1= ImageIO.read(new File("rsc/Beinr1.png")); ibeinr2 = ImageIO.read(new File("rsc/Beinr2.png"));
+			ib1 = ImageIO.read(new File("src/main/resources/b1.0.png"));
+			ib2 = ImageIO.read(new File("src/main/resources/b1.1.png"));
+			//Player
+			ihead = ImageIO.read(new File("src/main/resources/Head.png"));
+			ibody = ImageIO.read(new File("src/main/resources/Body.png"));
+			iarm1 = ImageIO.read(new File("src/main/resources/Arm1.png")); iarm2 = ImageIO.read(new File ("src/main/resources/Arm2.png"));
+			iarmr1 = ImageIO.read(new File("src/main/resources/Armr1.png")); iarmr2 = ImageIO.read(new File("src/main/resources/Armr2.png"));
+			ibein1 = ImageIO.read(new File("src/main/resources/Bein1.png")); ibein2 = ImageIO.read(new File ("src/main/resources/Bein2.png"));
+			ibeinr1= ImageIO.read(new File("src/main/resources/Beinr1.png")); ibeinr2 = ImageIO.read(new File("src/main/resources/Beinr2.png"));
 			//Feuerball
-			iball = ImageIO.read(new File("rsc/Angriff/Ball.png"));
-			ianball1 = ImageIO.read(new File("rsc/Animationen/AngriffBall1.png"));
-			ianball2 = ImageIO.read(new File("rsc/Animationen/AngriffBall2.png"));
-			
-			iflamme1 = ImageIO.read(new File("rsc/Animationen/flamme.1.png"));
-			iflamme2 = ImageIO.read(new File("rsc/Animationen/flamme.1.2.png"));
+			iball = ImageIO.read(new File("src/main/resources/Angriff/Ball.png"));
+			ianball1 = ImageIO.read(new File("src/main/resources/Animationen/AngriffBall1.png"));
+			ianball2 = ImageIO.read(new File("src/main/resources/Animationen/AngriffBall2.png"));
+
+			iflamme1 = ImageIO.read(new File("src/main/resources/Animationen/flamme.1.png"));
+			iflamme2 = ImageIO.read(new File("src/main/resources/Animationen/flamme.1.2.png"));
 			// Der etste Gegener
-			igegner1 = ImageIO.read(new File("rsc/gegner1.png"));
+			igegner1 = ImageIO.read(new File("src/main/resources/gegner1.png"));
 			//Schrott
-			ischrott1 = ImageIO.read(new File("rsc/schrott1.png"));
-			ischrott2 = ImageIO.read(new File("rsc/schrott2.png"));
-			ischrott3 = ImageIO.read(new File("rsc/schrott3.png"));
-			//Hindernisse 
-			ih001 = ImageIO.read(new File("rsc/Hindernisse/H_001.png"));
-			ihkaputt001 = ImageIO.read(new File ("rsc/Hindernisse/Hkaputt_001.png"));
-			//Grafische Anzeigen 
-			iAufladen1 = ImageIO.read(new File("rsc/GrapischeAnzeigen/Aufladenrot.png"));
-			iAufladen2 = ImageIO.read(new File("rsc/GrapischeAnzeigen/Aufladengruen.png"));
-			iMeter = ImageIO.read(new File("rsc/GrapischeAnzeigen/Meter.png"));			//Patickel
-			iEffeckt1 =  ImageIO.read(new File("rsc/PatickelEffeckte/Patickel1.png"));
-			//Herzen 
-			iherz1 = ImageIO.read(new File("rsc/GrapischeAnzeigen/Herz1.png"));
-			iherz2 = ImageIO.read(new File("rsc/GrapischeAnzeigen/Herz2.png"));
-			iherz3 = ImageIO.read(new File("rsc/GrapischeAnzeigen/Herz3.png"));
-			iherzleer1 = ImageIO.read(new File("rsc/GrapischeAnzeigen/Herzleer1.png"));
-			iherzleer2 = ImageIO.read(new File("rsc/GrapischeAnzeigen/Herzleer2.png"));
-			iherzleer3 = ImageIO.read(new File("rsc/GrapischeAnzeigen/Herzleer3.png"));
+			ischrott1 = ImageIO.read(new File("src/main/resources/schrott1.png"));
+			ischrott2 = ImageIO.read(new File("src/main/resources/schrott2.png"));
+			ischrott3 = ImageIO.read(new File("src/main/resources/schrott3.png"));
+			//Hindernisse
+			ih001 = ImageIO.read(new File("src/main/resources/Hindernisse/H_001.png"));
+			ihkaputt001 = ImageIO.read(new File ("src/main/resources/Hindernisse/Hkaputt_001.png"));
+			//Grafische Anzeigen
+			iAufladen1 = ImageIO.read(new File("src/main/resources/GrapischeAnzeigen/Aufladenrot.png"));
+			iAufladen2 = ImageIO.read(new File("src/main/resources/GrapischeAnzeigen/Aufladengruen.png"));
+			iMeter = ImageIO.read(new File("src/main/resources/GrapischeAnzeigen/Meter.png"));			//Patickel
+			iEffeckt1 =  ImageIO.read(new File("src/main/resources/PatickelEffeckte/Patickel1.png"));
+			//Herzen
+			iherz1 = ImageIO.read(new File("src/main/resources/GrapischeAnzeigen/Herz1.png"));
+			iherz2 = ImageIO.read(new File("src/main/resources/GrapischeAnzeigen/Herz2.png"));
+			iherz3 = ImageIO.read(new File("src/main/resources/GrapischeAnzeigen/Herz3.png"));
+			iherzleer1 = ImageIO.read(new File("src/main/resources/GrapischeAnzeigen/Herzleer1.png"));
+			iherzleer2 = ImageIO.read(new File("src/main/resources/GrapischeAnzeigen/Herzleer2.png"));
+			iherzleer3 = ImageIO.read(new File("src/main/resources/GrapischeAnzeigen/Herzleer3.png"));
 
 		} catch (IOException e) {
 			
